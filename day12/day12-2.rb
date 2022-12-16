fe4f7284856b58a4d5bcbc53615b8ab1802856e4
Bluedillydilly <dylancuprewich@gmail.com>
2022-12-15 19:25:40 -0500

    dfs part 1 + part 2

---
+++ day12/day12-2.rb
@@ -0,0 +1,93 @@
+require 'set'
+
+height_map = []
+candidates = []
+$goal = [0,0]
+visited = []
+$point_map = {}
+
+File.open("./day12/day12-input.txt").each {|line|
+    height_map.push(line.strip.split(''))
+}
+height_map.length.times do |y|
+    height_map[0].length.times do |x|
+        case height_map[y][x]
+        in "S" | "a"
+            candidates.push([y,x])
+            height_map[y][x] = "a".ord - "a".ord
+        in "E"
+            $goal = [y,x]
+            height_map[y][x] = "z".ord - "a".ord
+        else
+            height_map[y][x] = height_map[y][x].ord - "a".ord
+        end
+    end
+end
+height_map.length.times do |y|
+    height_map[0].length.times do |x|
+        current = height_map[y][x]
+        neighbors = []
+        if y > 0 && (height_map[y-1][x] - current) <= 1 # top valid
+            neighbors.push([y-1,x])
+        end
+        if x > 0 && (height_map[y][x-1] - current) <= 1 # left valid
+            neighbors.push([y,x-1])
+        end
+        if y < (height_map.length-1) && (height_map[y+1][x] - current) <= 1 # bottom valid
+            neighbors.push([y+1,x])
+        end
+        if x < (height_map[0].length-1) && (height_map[y][x+1] - current) <= 1 # right valid
+            neighbors.push([y,x+1])
+        end
+        $point_map[[y,x]] = neighbors
+    end
+end
+
+def reconstruct_path(cameFrom, current)
+    total_path = [current]
+    while cameFrom.key?(current)
+        current = cameFrom[current]
+        total_path.push(current)
+    end
+    return total_path
+end
+
+def DFS(start)
+    openSet = Set.new([start])
+    cameFrom = {}
+
+    gScore = {}
+    gScore.default = 10000000
+    gScore[start] = 0
+
+    while !openSet.empty?()
+        current = openSet.to_a[0]
+        if current == $goal
+            return reconstruct_path(cameFrom, current).length-1
+        end
+
+        openSet.delete(current)
+        $point_map[current].each do |neigh|
+            if (gScore[current] + 1) < gScore[neigh]
+                cameFrom[neigh] = current
+                gScore[neigh] = gScore[current] + 1
+                if !openSet.include?(neigh)
+                    openSet.add(neigh)
+                end
+            end
+        end
+    end
+
+    return 10000000
+end
+
+best = 1000000
+candidates.each do |start|
+    p start
+    result = DFS(start)
+    if  result < best
+        best = result
+    end
+end
+
+puts best
